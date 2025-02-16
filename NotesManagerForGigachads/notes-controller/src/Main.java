06e5c9af99f5c4d8caac3b020b0b16a5cad22c71
Sawtooth04 <andrey.y96@mail.ru>
2025-02-16 12:57:21 +0300

    doc

--- NotesManagerForGigachads/notes-controller/src/Main.java
+++ NotesManagerForGigachads/notes-controller/src/Main.java
@@ -1,4 +1,11 @@
+/**
+ * Основной класс слоя контроллеров
+ */
 public class Main {
+    /**
+     * Точка входа
+     * @param args массив строковых параметров, с которыми будет запущен слой
+     */
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
